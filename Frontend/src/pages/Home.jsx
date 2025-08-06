f95ccc4516d7954cacb0c96905ebd3e72a88cf13
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2025-08-06 20:09:13 +0530

    frontend

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,44 @@
 // import '../index.css'
 import toast from "react-hot-toast"
 import { Navbar } from "../Components/Navbar"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import RateLimitedUi from "../Components/RateLimitedUi"
-
+import axios from 'axios'
 
 export const Home = () => {
   const [isRateLimited, setRateLimited] = useState(false)
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>
+  {
+      const fetchNotes = async ()=>
+      {
+          try {
+          const res = await axios.get('http://localhost:5000/api/notes');
+          // const data = await res.json();
+          console.log(res.data);
+          setNotes(res.data);
+          setRateLimited(false);
+          } 
+          catch (error) {
+            console.log(error);
+            if(error.response.status===429)
+            {
+                setRateLimited(true);
+            }
+            else
+            {
+              toast.error("failed to load")
+            }
+          }
+          finally
+          {
+            setLoading(false);
+          }
+      }
+fetchNotes();
+  }, [])
 
   return (
     <div data-theme="forest" className="min-h-screen">
